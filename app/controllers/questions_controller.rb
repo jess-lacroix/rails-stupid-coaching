80fa967dcc8a734286f8de2ae6d95bd91a3c2ae4
Jessica Lacroix <jess.dengler@gmail.com>
2020-11-09 15:20:13 +0100

    rails

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,15 @@
 class QuestionsController < ApplicationController
-  def index
-  end
-
   def ask
   end
+
+  def answer
+    @question = params[:question]
+    if @question == "i am going to work"
+      @answer = "great"
+    elsif @question.include?("?") 
+      @answer = 'Silly question, get dressed and go to work!.'
+    else
+      @answer = 'I dont care, get dressed and go to work!'
+    end
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'questions/index'
   
-  get '/ask', to: 'questions_controller#ask'
+get 'ask', to: 'questions#ask'
+get 'answer', to: 'questions#answer'
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
