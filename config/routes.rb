180f1bcba6efb847779a826e33b230a7382649a2
Jessica Lacroix <jess.dengler@gmail.com>
2020-11-09 14:36:41 +0100

    rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'questions/index'
+  
+  get '/ask', to: 'questions_controller#ask'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
